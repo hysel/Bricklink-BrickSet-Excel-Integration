f785092332d62b1010c63294006a08fe1b86cd0f
Itamar Budin <itamarbudin@outlook.com>
2021-03-10 11:14:25 -0500

    Add project files.

---
+++ BrickLink.cs
@@ -0,0 +1,272 @@
+﻿// ***************************************************************************************
+// * BrickLink Excel function intgration 
+// * Version 1.0 3/10/2021
+// * Itamar Budin lego.c.israel@gmail.com
+// * Using code samples from multiple resoruce (see internal comments for reffrence) 
+// ***************************************************************************************
+
+
+using Nancy.Helpers;
+using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+namespace BrickLink
+{
+    public static class BricklinkExcelIntegration
+    {
+        const string consumerKey = "";
+        const string consumerSecret = "";
+        const string tokenValue = "";
+        const string tokenSecret = "";
+        public static string tokenx = "";
+
+        private static string Escape(string s)
+        {
+            string[] charsToEscape = new[] { "!", "*", "'", "(", ")" };
+            StringBuilder escaped = new StringBuilder(Uri.EscapeDataString(s));
+            foreach (var t in charsToEscape)
+            {
+                escaped.Replace(t, Uri.HexEscape(t[0]));
+            }
+            return escaped.ToString();
+        }
+        private static readonly string[] UriRfc3986CharsToEscape = new[] { "!", "*", "'", "(", ")" };
+
+        private static string EscapeUriDataStringRfc3986(string value)
+        {
+            StringBuilder escaped = new StringBuilder(Uri.EscapeDataString(value));
+            for (int i = 0; i < UriRfc3986CharsToEscape.Length; i++)
+            {
+                escaped.Replace(UriRfc3986CharsToEscape[i], Uri.HexEscape(UriRfc3986CharsToEscape[i][0]));
+            }
+            return escaped.ToString();
+        }
+
+        public static string GetSetInformation(string url, string requestType)
+        {
+            if (requestType != "info")
+            {
+                url += "/price";
+            }
+
+            var httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
+            httpWebRequest.Method = "GET";
+            httpWebRequest.KeepAlive = false;
+            httpWebRequest.ServicePoint.Expect100Continue = false;
+
+            string timeStamp = ((int)(DateTime.UtcNow - new DateTime(1970, 1, 1)).TotalSeconds).ToString();
+            string nonce = Convert.ToBase64String(Encoding.UTF8.GetBytes(timeStamp));
+
+
+            string signatureBaseString = Escape(httpWebRequest.Method.ToUpper()) + "&";
+            signatureBaseString += EscapeUriDataStringRfc3986(url) + "&";
+            signatureBaseString += EscapeUriDataStringRfc3986(
+                "oauth_consumer_key=" + EscapeUriDataStringRfc3986(consumerKey) + "&" +
+                "oauth_nonce=" + EscapeUriDataStringRfc3986(nonce) + "&" +
+                "oauth_signature_method=" + EscapeUriDataStringRfc3986("HMAC-SHA1") + "&" +
+                "oauth_timestamp=" + EscapeUriDataStringRfc3986(timeStamp) + "&" +
+                "oauth_token=" + EscapeUriDataStringRfc3986(tokenValue) + "&" +
+                "oauth_version=" + EscapeUriDataStringRfc3986("1.0"));
+            Console.WriteLine(@"signatureBaseString: " + signatureBaseString);
+
+            string key = EscapeUriDataStringRfc3986(consumerSecret) + "&" + EscapeUriDataStringRfc3986(tokenSecret);
+            Console.WriteLine(@"key: " + key);
+            var signatureEncoding = new ASCIIEncoding();
+            byte[] keyBytes = signatureEncoding.GetBytes(key);
+            byte[] signatureBaseBytes = signatureEncoding.GetBytes(signatureBaseString);
+            string signatureString;
+            using (var hmacsha1 = new HMACSHA1(keyBytes))
+            {
+                byte[] hashBytes = hmacsha1.ComputeHash(signatureBaseBytes);
+                signatureString = Convert.ToBase64String(hashBytes);
+            }
+            signatureString = EscapeUriDataStringRfc3986(signatureString);
+            Console.WriteLine(@"signatureString: " + signatureString);
+
+            string SimpleQuote(string s) => '"' + s + '"';
+
+            string header = "OAuth oauth_consumer_key=" + SimpleQuote(consumerKey) + ",oauth_token=" + SimpleQuote(tokenValue) + ",oauth_signature_method=" + SimpleQuote("HMAC-SHA1") + ",oauth_timestamp=" + SimpleQuote(timeStamp) + ",oauth_nonce=" + SimpleQuote(nonce) + ",oauth_version=" + SimpleQuote("1.0") + ",oauth_signature=" + SimpleQuote(signatureString);
+
+            Console.WriteLine(@"header: " + header);
+            httpWebRequest.Headers.Add(HttpRequestHeader.Authorization, header);
+            var response = httpWebRequest.GetResponse();
+            string characterSet = ((HttpWebResponse)response).CharacterSet;
+            var responseEncoding = characterSet == ""
+                ? Encoding.UTF8
+                : Encoding.GetEncoding(characterSet ?? "utf-8");
+            var responsestream = response.GetResponseStream();
+            if (responsestream == null)
+            {
+                throw new ArgumentNullException(nameof(characterSet));
+            }
+            using (responsestream)
+            {
+                var reader = new StreamReader(responsestream, responseEncoding);
+                string result = reader.ReadToEnd();
+                Console.WriteLine(@"result: " + result);
+
+
+                Regex tid = new Regex("oauth_token=(.*?)&");
+                Match match = tid.Match(result);
+                tokenx = match.Groups[1].Value;
+                Console.WriteLine(match.Groups[1].Value);
+                return result;
+            }
+
+        }
+
+        public static string GetSetName(string name)
+        {
+            string setInformation = GetSetInformation(GetURL(name), "info");
+            var obj = JObject.Parse(setInformation);
+            if (!obj.ToString().Contains("TIMESTAMP"))
+            {
+                string value = (string)obj["data"]["name"];
+                return value != null ? HttpUtility.HtmlDecode(value) : "no results";
+            }
+            else
+            {
+                Thread.Sleep(500);
+                return GetSetName(name);
+            }
+        }
+
+        public static string GetSetImage(string name)
+        {
+            string setInformation = GetSetInformation(GetURL(name), "info");
+            var obj = JObject.Parse(setInformation);
+            if (!obj.ToString().Contains("TIMESTAMP"))
+            {
+                string value = (string)obj["data"]["thumbnail_url"];
+                if (value != null)
+                {
+                    return "http:" + value;
+                }
+                else
+                {
+                    return "no results";
+                }
+            }
+            else
+            {
+                Thread.Sleep(500);
+                return GetSetImage(name);
+            }
+
+        }
+
+        public static string GetSetYear(string name)
+        {
+            string setInformation = GetSetInformation(GetURL(name), "info");
+            var obj = JObject.Parse(setInformation);
+            if (!obj.ToString().Contains("TIMESTAMP"))
+            {
+                string value = (string)obj["data"]["year_released"];
+                if (value != null)
+                {
+                    return value;
+                }
+                else
+                {
+                    return "no results";
+                }
+            }
+            else
+            {
+                Thread.Sleep(500);
+                return GetSetYear(name);
+            }
+
+        }
+        public static string GetSetPrice(string name)
+        {
+            string setInformation = GetSetInformation(GetURL(name), "price");
+            var obj = JObject.Parse(setInformation);
+            if (!obj.ToString().Contains("TIMESTAMP"))
+            {
+                string value = (string)obj["data"]["avg_price"];
+                if (value != null)
+                {
+                    return value;
+                }
+                else
+                {
+                    return "no results";
+                }
+            }
+            else
+            {
+                Thread.Sleep(500);
+                return GetSetPrice(name);
+            }
+        }
+
+        private static string GetURL(string name)
+        {
+            string url;
+            string setTypeValue = GetSetType(name);
+            if (setTypeValue == "SET")
+            {
+                if (Char.IsLetter(name[0]))
+                {
+                    url = "https://api.bricklink.com/api/store/v1/items/set/" + name;
+                }
+                else
+                {
+                    url = "https://api.bricklink.com/api/store/v1/items/set/" + name + "-1";
+                }
+            }
+            else if (setTypeValue == "GEAR")
+            {
+                url = "https://api.bricklink.com/api/store/v1/items/gear/" + name;
+            }
+            else
+            {
+                url = "https://api.bricklink.com/api/store/v1/items/minifig/" + name;
+            }
+            return url;
+        }
+
+        private static string GetSetType(string name)
+        {
+
+            WebClient myWebClient = new WebClient();
+            string responseBodySet = myWebClient.DownloadString("https://www.bricklink.com/v2/catalog/catalogitem.page?S=" + name);
+            string responseBodyGear = myWebClient.DownloadString("https://www.bricklink.com/v2/catalog/catalogitem.page?G=" + name);
+            string returnValue = "";
+
+            if ((responseBodySet.Contains("BrickLink Page Not Found") == false)){
+                if (responseBodySet.Contains("BrickLink - Oops, Sorry!") == false)
+                {
+                    returnValue = "SET";
+                }            
+            }
+            else if ((responseBodyGear.Contains("BrickLink Page Not Found") == false))
+            {
+                if (responseBodyGear.Contains("BrickLink - Oops, Sorry!") == false)
+                {
+                    returnValue = "GEAR";
+                }
+            }
+            else
+            {
+                returnValue = "MINI";
+            }
+            return returnValue;
+
+        }
+
+        public static void Main()
+        {
+
+        }
+
+    }
+
+}
